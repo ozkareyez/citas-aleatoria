4d302f1934f6c028aa1e60ebc62916a65229dd5b
oscar reyes <ozkareyez@gmail.com>
2025-10-03 17:21:23 -0500

    inicio

---
+++ citas.js
@@ -0,0 +1,143 @@
+let citas = [
+  {
+    'autor': 'Albert Einstein',
+    'texto': 'No poseo talentos especiales. Solo soy apasionadamente curioso.'
+  },
+  {
+    'autor': 'Anónimo',
+    'texto': 'Semanas de programación te pueden ahorrar horas de planeación.'
+  },
+  {
+    'autor': 'Alan Kay',
+    'texto': 'La mejor forma de predecir el futuro es inventarlo.'
+  },
+  {
+    'autor': 'Amelia Earhart',
+    'texto': 'Lo más dificil es tomar la decisión de actuar. El resto es simplemente tenacidad.'
+  },
+  {
+    'autor': 'Aristotle',
+    'texto': 'La calidad no es un acto, es un hábito.'
+  },
+  {
+    'autor': 'Benjamin Franklin',
+    'texto': 'Dímelo y lo olvidaré. Enséñamelo y lo recordaré. Involúcrame y lo aprenderé.'
+  },
+  {
+    'autor': 'Charles R. Swindoll',
+    'texto': 'La vida es 10% lo que te ocurre y 90% cómo reaccionas.'
+  },
+  {
+    'autor': 'Jane Goodall',
+    'texto': 'Lo que haces hace una diferencia. Y debes decidir si qué tipo de diferencia quieres hacer.'
+  },
+  {
+    'autor': 'John Muir',
+    'texto': 'El poder de la imaginación nos hace infinitos.'
+  },
+  {
+    'autor': 'Mark Twain',
+    'texto': 'Los dos días más importantes de tu vida son el día que naciste y el día que descubres por qué.'
+  },
+ {
+    "texto": "Porque tanto amó Dios al mundo que dio a su Hijo unigénito, para que todo el que cree en él no se pierda, sino que tenga vida eterna.",
+    "autor": "Juan 3:16"
+  },
+  {
+    "texto": "El Señor es mi pastor, nada me falta.",
+    "autor": "Salmos 23:1"
+  },
+  {
+    "texto": "Y sabemos que Dios dispone todas las cosas para el bien de los que lo aman, es decir, de los que él ha llamado de acuerdo a su propósito.",
+    "autor": "Romanos 8:28"
+  },
+  {
+    "texto": "No tengas miedo, que yo estoy contigo; no te desanimes, que yo soy tu Dios. Te daré fuerzas, y te ayudaré; te sostendré con mi mano victoriosa.",
+    "autor": "Isaías 41:10"
+  },
+  {
+    'texto': "No temas, porque yo estoy contigo; no desmayes, porque yo soy tu Dios.",
+    "autor": "Isaías 41:10"
+
+  },
+  {
+    "texto": "Jehová te guardará de todo mal; él guardará tu alma",
+    "autor": "Salmo 121:7 "
+  },
+  {
+    "texto":"Y todo lo que pídales en oración, creyendo, lo recibirán",
+    "autor":"Mateo 21:22"
+
+  },
+
+  {
+    "texto":"Y si alguna cosa hemos pecado, abogado tenemos para con el Padre, a Jesucristo el justo" ,
+    "autor":"1 Juan 2:1"
+  },
+
+{
+    "texto":"Pues si ustedes, que son malos, saben dar cosas buenas a sus hijos, ¡cuánto más el Padre celestial dará el Espíritu Santo a quienes se lo pidan!",
+
+    "autor":"Lucas 11:13"
+  },
+  {
+    "texto":"El ladrón no viene más que a robar, matar y destruir; yo he venido para que tengan vida y la tengan en abundancia",
+
+    "autor":"Juan 10:10"
+  },
+  {
+    "texto":"Si alguno de ustedes requiere de sabiduría, pídasela a Dios, y él se la dará, pues Dios se la da a todos en abundancia y sin hacer ningún reproche",
+
+    "autor":"Santiago 1:5"
+  },
+
+  {
+    "texto":"Y ahora permanecen la fe, la esperanza y el amor, estos tres; pero el mayor de ellos es el amor", 
+    "autor": "1 Corintios 13:13"
+
+  },
+
+  {
+
+    "texto":"Es, pues, la fe la certeza de lo que se espera, la convicción de lo que no se ve",
+    "autor":"Hebreos 11:1"
+  },
+  {
+
+    "texto":"Si me amáis, guardad mis mandamientos",
+    "autor":"Juan 14:15"
+  },
+
+  {
+
+    "texto":"El que no ama, no ha conocido a Dios; porque Dios es amor",
+    "autor":"1 Juan 4:8"
+  },
+
+{
+
+    "texto":"Pero Dios, que es rico en misericordia, por su gran amor por nosotros, nos dio vida con Cristo, aun cuando estábamos muertos en pecados. ¡Por gracia ustedes han sido salvados! ",
+    "autor":"Efesios 2:4-5"
+  },
+  {
+
+    "texto":"En todo caso, cada uno de ustedes ame también a su esposa como a sí mismo, y que la esposa respete a su esposo ",
+    "autor":"Efesios 5:33"
+  },
+  {
+
+    "texto":"Porque a vosotros os es concedido a causa de Cristo, no sólo que creáis en él, sino también que padezcáis por él",
+    "autor":" Filipenses 1:29"
+  },
+  {
+
+    "texto":"Dios es amor. El que permanece en amor, permanece en Dios, y Dios en él",
+    "autor":"1 Juan 4:16"
+  },
+  {
+
+    "texto":"Y la esperanza no avergüenza, porque el amor de Dios ha sido derramado en nuestros corazones por el Espíritu Santo que nos fue dado ",
+    "autor":"Romanos 5:5"
+  },
+
+];
